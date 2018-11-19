65930e04cd8f98f409101f6e6096e48303936f08
Dmytro Panasiuk <dmitriy.panasyuk@gmail.com>
2018-11-19 14:48:33 +0200

    Create migration mechanism

--- app/routes.py
+++ app/routes.py
@@ -22,7 +22,7 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'''Login requested for user={form.username.data},
-                remember_me={form.remember_me.data}''')
+        flash('Login requested for user={}, remember_me={}'
+              .format(form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
